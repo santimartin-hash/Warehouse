a035413e8e6309be04d4345bb5521f78ff3bb4bd
Santiago Martin <Santiago Martin@HZ114819.H012108.NET>
2023-01-10 10:03:43 +0100

    definida clase refresco

---
+++ src/Clases/Refresco.java
@@ -0,0 +1,36 @@
+package Clases;
+
+public class Refresco {
+private String sabor;
+private boolean zumo;
+private boolean gaseoso;
+private int cantidadAzucar;
+public String getSabor() {
+	return sabor;
+}
+public void setSabor(String sabor) {
+	this.sabor = sabor;
+}
+public boolean isZumo() {
+	return zumo;
+}
+public void setZumo(boolean zumo) {
+	this.zumo = zumo;
+}
+public boolean isGaseoso() {
+	return gaseoso;
+}
+public void setGaseoso(boolean gaseoso) {
+	this.gaseoso = gaseoso;
+}
+public int getCantidadAzucar() {
+	return cantidadAzucar;
+}
+public void setCantidadAzucar(int cantidadAzucar) {
+	this.cantidadAzucar = cantidadAzucar;
+}
+
+public void visualizarPropiedades() {
+
+}
+}
