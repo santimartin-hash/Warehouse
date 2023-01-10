1307601b42aafe1db0c73183ae8afb83f14fbd32
Santiago Martin <Santiago Martin@HZ114819.H012108.NET>
2023-01-10 10:04:00 +0100

    definida clase Articulo

---
+++ src/Clases/Articulo.java
@@ -0,0 +1,54 @@
+package Clases;
+
+public abstract class Articulo {
+	private String code;
+	private String name;
+	private String mark;
+	private int capacidadBotella;
+	private double precio;
+	private int stock;
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMark() {
+		return mark;
+	}
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+	public int getCapacidadBotella() {
+		return capacidadBotella;
+	}
+	public void setCapacidadBotella(int capacidadBotella) {
+		this.capacidadBotella = capacidadBotella;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
+	public void incrementarStock(int Stock) {};
+	public void disminuirStock(int Stock) {};
+	public void visualizarArticulo() {};
+	public boolean saludable() {return false;};
+	public void preciototal() {};
+	
+}
