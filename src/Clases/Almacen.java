3985253525a338c32f453fa6a4bfab7c6ded345e
Santiago Martin <Santiago Martin@HZ114819.H012108.NET>
2023-01-12 12:44:33 +0100

    clase almacen

---
+++ src/Clases/Almacen.java
@@ -0,0 +1,45 @@
+package Clases;
+
+import java.util.ArrayList;
+
+public class Almacen {
+	public void cargarDatos () {
+		
+	}
+	
+	public Articulo elMasCaro() {
+		return null;
+	}
+	
+	public double precio (String codigoProducto) {
+		return 0;
+	}
+	
+	public boolean hayStock(String codigoProducto ) {
+		return false;	
+	}
+	
+	public ArrayList<Articulo> stockJusto ( ) {
+		return null;	
+	}
+	
+	public Articulo articulo (String codigoProducto) {
+		return null;	
+	}
+	
+	public boolean disponibilidad (int cantidad, String codigoProducto ) {
+		return false;
+	}
+	
+	public ArrayList<Articulo> equivalentes ( ) {
+		return null;	
+	}
+	
+	public ArrayList<Articulo> ordenarPorprecio ( ) {
+		return null;	
+	}
+	
+	public void ordenarPorStock (String orden) {
+		
+	}
+}
